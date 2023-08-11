09ae69f52c1ae4a051a3286c4fc01117a81d6a0c
hartmandy <120344775+hartmandy@users.noreply.github.com>
2023-08-11 18:37:07 -0400

    has errors, but adding usememo

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useMemo } from "react";
 
-import './App.css';
-import DemoList from './components/Demo/DemoList';
-import Button from './components/UI/Button/Button';
+import "./App.css";
+import DemoList from "./components/Demo/DemoList";
+import Button from "./components/UI/Button/Button";
 
 function App() {
-  const [listTitle, setListTitle] = useState('My List');
+  const [listTitle, setListTitle] = useState("My List");
 
   const changeTitleHandler = useCallback(() => {
-    setListTitle('New Title');
+    setListTitle("New Title");
   }, []);
 
+  const listItems = (useMemo = (() => [5, 3, 1, 10, 9], []));
+
   return (
-    <div className='app'>
-      <DemoList title={listTitle} items={[5, 3, 1, 10, 9]} />
+    <div className="app">
+      <DemoList title={listTitle} items={listItems} />
       <Button onClick={changeTitleHandler}>Change List Title</Button>
     </div>
   );
--- src/components/Demo/DemoList.js
+++ src/components/Demo/DemoList.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useMemo } from "react";
 
-import classes from './DemoList.module.css';
+import classes from "./DemoList.module.css";
 
 const DemoList = (props) => {
-  const sortedList = props.items.sort((a, b) => a - b);
+  const { items } = props;
+
+  const sortedList = useMemo(() => {
+    return props.items.sort((a, b) => a - b);
+  }, [items]);
 
   return (
     <div className={classes.list}>
